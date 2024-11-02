560e610e65923006fff07ace1d4fa8fc93953225
Mohamed Nahli <mnahli@c2r3p7.1337.ma>
2024-11-02 13:28:26 +0100

    atoi

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mnahli <mnahli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 13:28:05 by mnahli            #+#    #+#             */
-/*   Updated: 2024/10/31 21:48:46 by mnahli           ###   ########.fr       */
+/*   Updated: 2024/11/02 11:23:15 by mnahli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,39 @@
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+    size_t i;
+    size_t j;
+    size_t need_len = ft_strlen(needle);
+    
+    i = 0;
+    if (needle[i] == '\0')
+        return ((char *)haystack);
+
+    while (haystack[i] && i < len)
+    {
+        j = 0;
+        while (haystack[i] == needle[j] && j < need_len)
+        {
+            i++;
+            j++;
+        }
+        // if (j == need_len)
+        if (needle[j] == '\0')
+            return ((char *)haystack + i - j);
+        i++;
+    }
+    return(NULL);
     
 }
+
+// int main()
+// {
+//     char haystack[] = "i lean c language in 42 school";
+//     char needle[] = "leag";
+//     size_t len = 50;
+    
+//     printf("%s\n", ft_strnstr(haystack, needle, len));
+//     printf("%s\n", strnstr(haystack, needle, len));
+//     printf("%p\n", ft_strnstr(haystack, needle, len));
+//     printf("%p\n", strnstr(haystack, needle, len));
+// }
